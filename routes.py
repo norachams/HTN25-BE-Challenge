727691ca24ab8007a31046b62a7ac4207893fc6f
nora chams <nchamseddin@gmail.com>
2025-02-10 23:41:34 -0500

    added updating user endpoint

--- routes.py
+++ routes.py
@@ -1,5 +1,7 @@
-from flask import Blueprint, jsonify
+from flask import Blueprint, jsonify, request
 from models import db, User, Scan, Activity
+from datetime import datetime, timezone
+
 
 routes = Blueprint("routes", __name__)
 
@@ -8,6 +10,7 @@ def get_users():
     users = User.query.all()
     
     user_list = []
+    
     for user in users:
         user_data = {
             "name": user.name,
@@ -35,7 +38,6 @@ def get_user(badge_code):
     if not user:
         return jsonify({"error": "User not found"}), 404
 
-    
     user_data = {
         "name": user.name,
         "email": user.email,
@@ -52,3 +54,47 @@ def get_user(badge_code):
         ]
     }
     return jsonify(user_data)
+
+
+@routes.route('/users/<string:badge_code>', methods=['PUT'])
+def update_user(badge_code):
+
+    user = User.query.filter_by(badge_code=badge_code).first()
+    if not user:
+        return jsonify({"error": "User not found"}), 404
+
+    data = request.get_json()
+    if not data:
+        return jsonify({"error": "No data provided"}), 400
+
+    if "name" in data:
+        user.name = data["name"]
+    if "phone" in data:
+        user.phone = data["phone"]
+
+    #updating the feild manually to be safe
+    user.updated_at = datetime.now(timezone.utc)
+
+    try:
+        db.session.commit()
+    except Exception as e:
+        db.session.rollback()
+        return jsonify({"error": "Error updating user", "details": str(e)}), 500
+
+    # update data and reuturn it 
+    updated_user_data = {
+        "name": user.name,
+        "email": user.email,
+        "phone": user.phone,
+        "badge_code": user.badge_code,
+        "updated_at": user.updated_at.isoformat(),
+        "scans": [
+            {
+                "activity_name": scan.activity.name,
+                "activity_category": scan.activity.category,
+                "scanned_at": scan.scanned_at.isoformat()
+            }
+            for scan in user.scans
+        ]
+    }
+    return jsonify(updated_user_data)
